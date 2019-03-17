adb3ac991cdac8c035f31a150f64a8af9525d5fe
John Grey <jgrey@ucsc.edu>
2019-03-17 05:15:59 +0000

    adds a plain text exporter for bookmarks

---
+++ bkmkorg/plain_exporter.py
@@ -0,0 +1,29 @@
+"""
+A Plain Text exporter for bookmarks
+"""
+import logging as root_logger
+from util import bookmarkTuple
+
+logging = root_logger.getLogger(__name__)
+
+def tuple_to_str(bkmk_tuple):
+    return "{} ||| :{}: ||| {}".format(bkmk_tuple.name,
+                                     ":".join(bkmk_tuple.tags),
+                                     bkmk_tuple.url)
+
+
+def exportBookmarks(data, filename):
+    """ Main function. Takes a trie of data and outputs a string """
+    #dfs the trie
+    #print out the items that are bookmark tuples
+    logging.info("Converting data to text")
+
+    frontier = [data]
+    with open(filename, 'a') as f:
+        while bool(frontier):
+            current = frontier.pop()
+            if isinstance(current, bookmarkTuple):
+                theString = tuple_to_str(current) + "\n"
+                f.write(theString)
+            elif isinstance(current, dict):
+                frontier += list(current.values())
