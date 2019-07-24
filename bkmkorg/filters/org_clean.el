379994c5c82ea710dc30f3175c5be294c4db5e06
John Grey <jgrey@ucsc.edu>
2019-07-24 20:00:04 +0100

    adds stub of org clean emacs script

---
+++ bkmkorg/filters/org_clean.el
@@ -0,0 +1,16 @@
+;; Lisp Org Cleaner
+
+;; select directory
+;; get org files
+
+;; visit each file
+;; ;; indent buffer
+;; ;; get all links + line + indent
+;; ;; remove duplicates
+;; ;; get users
+;; ;; make user links
+;; ;; keyword whitelist
+
+;; get tweets with videos
+;; download videos to appropriate media dir
+
