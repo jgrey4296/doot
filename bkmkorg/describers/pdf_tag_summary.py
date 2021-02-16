6361e432dcea0852ff2fb3b0f58b376d56db6b5b
John Grey <johngrey4296@gmail.com>
2021-02-16 16:23:51 +0000

    add automatic pdf summary generator using tags

---
+++ bkmkorg/describers/pdf_tag_summary.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python3
+# https://mypy.readthedocs.io/en/stable/cheat_sheet_py3.html
+from typing import List, Set, Dict, Tuple, Optional, Any
+from typing import Callable, Iterator, Union, Match
+from typing import Mapping, MutableMapping, Sequence, Iterable
+from typing import cast, ClassVar, TypeVar, Generic
+
+import bibtexparser as b
+from datetime import datetime
+
+from os import listdir
+from os.path import join, isfile, exists, abspath
+from os.path import split, isdir, splitext, expanduser
+import argparse
+import logging as root_logger
+
+from bkmkorg.utils import retrieval
+from bkmkorg.utils import bibtex as BU
+from bkmkorg.utils import pdf as PU
+
+def custom_parse(record):
+    if 'year' not in record:
+        year_temp = "2020"
+    else:
+        year_temp = record['year']
+    if "/" in year_temp:
+        year_temp = year_temp.split("/")[0]
+
+    year = datetime.strptime(year_temp, "%Y")
+    tags = []
+    if 'tags' in record:
+        tags = [x.strip() for x in record['tags'].split(",")]
+
+    record['year'] = year
+    record['tags'] = tags
+
+    return record
+
+if __name__ == "__main__":
+    # Setup root_logger:
+    LOGLEVEL = root_logger.DEBUG
+    LOG_FILE_NAME = "log.{}".format(splitext(split(__file__)[1])[0])
+    root_logger.basicConfig(filename=LOG_FILE_NAME, level=LOGLEVEL, filemode='w')
+
+    console = root_logger.StreamHandler()
+    console.setLevel(root_logger.INFO)
+    root_logger.getLogger('').addHandler(console)
+    logging = root_logger.getLogger(__name__)
+    ##############################
+    #see https://docs.python.org/3/howto/argparse.html
+    parser = argparse.ArgumentParser(formatter_class=argparse.RawDescriptionHelpFormatter,
+                                     epilog = "\n".join([""]))
+    parser.add_argument('--tag')
+    parser.add_argument('--target', action='append')
+    parser.add_argument('--output')
+    parser.add_argument('--bound', default=200)
+
+    args = parser.parse_args()
+    args.output = abspath(expanduser(args.output))
+
+    output_path = join(args.output, "{}_summary".format(args.tag))
+
+    bibtex_files = retrieval.get_data_files(args.target, ".bib")
+    db = b.bibdatabase.BibDatabase()
+    BU.parse_bib_files(bibtex_files, func=custom_parse, database=db)
+
+    entries_with_tag = [x for x in db.entries if args.tag in x['tags']]
+    entries_by_year = sorted(entries_with_tag, key=lambda x: x['year'])
+    pdfs_to_process = [x['file'] for x in entries_by_year]
+    expanded_paths = [abspath(expanduser(x)) for x in pdfs_to_process]
+    logging.info("Summarising {} pdfs".format(len(expanded_paths)))
+    PU.summarise_pdfs(expanded_paths, output=output_path, bound=args.bound)
