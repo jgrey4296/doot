6da23a3e372383658a500df11b50c6c671a02451
John Grey <johngrey4296@gmail.com>
2021-06-19 20:39:03 +0100

    implement url expander

--- bkmkorg/utils/twitter/url_expander.py
+++ bkmkorg/utils/twitter/url_expander.py
@@ -6,46 +6,70 @@ from os.path import (abspath, exists, expanduser, isdir, isfile, join, split,
                      splitext)
 
 import requests
-from bkmkorg/utils.file.retrieval import get_data_files
+from bkmkorg.utils.file.retrieval import get_data_files
+
+# Setup root_logger:
+from os.path import splitext, split
+import logging as root_logger
+LOGLEVEL = root_logger.DEBUG
+LOG_FILE_NAME = "log.{}".format(splitext(split(__file__)[1])[0])
+root_logger.basicConfig(filename=LOG_FILE_NAME, level=LOGLEVEL, filemode='w')
+
+console = root_logger.StreamHandler()
+console.setLevel(root_logger.INFO)
+root_logger.getLogger('').addHandler(console)
+logging = root_logger.getLogger(__name__)
+##############################
 
 parser = argparse.ArgumentParser(formatter_class=argparse.RawDescriptionHelpFormatter,
                                  epilog = "\n".join(["Index all tags found in orgs"]))
-parser.add_argument('--target', action="append")
+parser.add_argument('--target')
+parser.add_argument('--count', type=int, default=10)
 parser.add_argument('--output')
 
 SHORT_URL_RE = re.compile()
 
 if __name__ == '__main__':
     args = parser.parse_args()
-    args.target = [abspath(expanduser(x)) for x in args.target]
+    args.target = abspath(expanduser(args.target))
     args.output = abspath(expanduser(args.output))
 
-    targets = get_data_files(args.target, ext=".org")
-    all_urls = set()
+    unexpanded = []
+    expanded   = {}
 
-    for filename in targets:
-        # read in
-        lines = []
-        with file(filename, 'r') as f:
-            lines = f.readlines()
+    # load the targets
+    logging.info(f"Loading {args.target}")
+    with open(args.target, 'r') as f:
+        unexpanded = f.readlines()
 
-        outlines = []
-        for line in lines:
-            match = SHORT_URL_RE.match(line)
-            if not bool(match):
-                outlines.append(line)
-                continue
+    # load the output
+    logging.info(f"Loading {args.output}")
+    temp_lines = []
+    with open(args.output, 'r') as f:
+        temp_lines = f.readlines()
 
-            # Expand the url using a request
+    for line in temp_lines:
+        parts = line.split(":")
+        expanded[parts[0].strip()] = parts[1].strip()
 
-            all_urls.add()
-            outlines.append()
+    logging.info(f"Found {len(unexpanded)} : {len(expanded)}")
 
-        # Write out the transformed file
-        total_str = "\n".join(outlines)
-        with open(filename, 'w') as f:
-            f.write(total_str)
+    count = 0
+    while count < args.count and bool(unexpanded):
+        current = unexpanded.pop(0)
+        if current in expanded:
+            continue
 
+        response = requests.head(current, allow_redirects=True)
+        if response.ok:
+            expanded[current] = response.url
+        else:
+            expanded[current] = response.status_code
+
+        logging.info(f"Response for {current} : {expanded[current]}")
+        count += 1
+
+    to_string = "\n".join(["{} : {}".format(x,y) for x,y in expanded.items()])
 
     with open(args.output, 'w') as f:
-        f.write("\n".join(all_urls))
+        f.write(to_string)
