cd34a0184aaaa67965171ba172b1d12e7208c0ec
John Grey <johngrey4296@gmail.com>
2022-07-17 06:13:19 +0100

    [adb] get adb unify working

--- bkmkorg/integrators/backup/adb_unify.py
+++ bkmkorg/integrators/backup/adb_unify.py
@@ -5,6 +5,7 @@ import abc
 import argparse
 import logging as logmod
 import subprocess
+import pathlib
 from configparser import ConfigParser
 from copy import deepcopy
 from dataclasses import InitVar, dataclass, field
@@ -25,20 +26,23 @@ from weakref import ref
 
 from bkmkorg import DEFAULT_CONFIG, DEFAULT_SECRETS
 
-data_path    = files(DEFAULT_CONIFG)
+##-- argparse
+data_path    = files(f"bkmkorg.{DEFAULT_CONFIG}")
 data_secrets = data_path.joinpath(DEFAULT_SECRETS)
 
 parser = argparse.ArgumentParser(formatter_class=argparse.RawDescriptionHelpFormatter,
                                  epilog = "\n".join(["Walk through a library directory, and either push to,",
                                                      "or pull from, a tablet to sync the directories"]))
-parser.add_argument('--config', default=data_secrets)
-parser.add_argument('--library', required=True)
-parser.add_argument('--target',  required=True)
+parser.add_argument('--config', default=data_secrets, help="Has a default")
+parser.add_argument('--library', required=True, help="The local directory to use")
+parser.add_argument('--target',  required=True, help="The remote directory to use")
 parser.add_argument('--to-device',   action='store_true')
 parser.add_argument('--from-device', action='store_true')
+parser.add_argument('--depthskip', default=1, type=int)
+parser.add_argument('--id')
 parser.add_argument('--wait', type=int, default=10)
-
-
+##-- End argparse
+##-- Logging
 DISPLAY_LEVEL = logmod.INFO
 LOG_FILE_NAME = "log.{}".format(splitext(split(__file__)[1])[0])
 LOG_FORMAT    = "%(asctime)s | %(levelname)8s | %(message)s"
@@ -58,85 +62,105 @@ logger.addHandler(console_handler)
 logger.addHandler(file_handler)
 logging = logger
 logging.setLevel(logmod.DEBUG)
-##############################
+##-- End Logging
 
 
+# TODO use asyncio for subprocess control
 def expander(path):
     return abspath(expanduser(path))
 
 
-def dfs_dir(initial, sleep_time, func):
-    logging.info("Getting Data Files")
-    initial = initial if isinstance(initial, list) else [initial]
-    queue = [(x, x) for x in initial]
-    while bool(queue):
-        rel_path, abs_path = queue.pop(0)
-        if isdir(abs_path):
-            sub = [(join(rel_path, x), join(abs_path,x)) for x in listdir(abs_path)]
-            queue += sub
-            logging.info("Running on %s", abs_path)
-            func(rel_path)
-            sleep(sleep_time)
+def skip_depth_under(path, curr, depth=1):
+    up_by_depth = path/curr
+    for x in range(depth):
+        up_by_depth = up_by_depth.parent
 
-def push_sync(target, lib, current):
+    return up_by_depth < path
+
+def dfs_dir(initial_path, sleep_time, func):
+    logging.info("Getting Data Files for: %s", initial_path)
+    logging.info("--------------------")
+    initial = initial_path if isinstance(initial_path, list) else [initial_path]
+    queue = [x for x in initial]
+    while bool(queue):
+        curr_path = queue.pop(0)
+        sub = [x for x in curr_path.iterdir() if x.is_dir()]
+        queue += sub
+        logging.info("Running on %s", curr_path.relative_to(initial_path))
+        logging.info("--------------------")
+        func(curr_path.relative_to(initial_path))
+        sleep(sleep_time)
+
+def push_sync(device_id, target, lib, current, depth=1):
     """
     Run an adb push command to sync libdir with the target
     `target` : base path on device
     `lib`    : base path on source
     `current`: current pos relative to `lib`
     """
-    logging.info("Pushing: %s : %s : %s", join(target, current), join(lib, current), current)
+    if skip_depth_under(lib, current, depth):
+        logging.info("Skipping %s", current)
+        return
+
+    logging.info("Pushing: %s\nto: %s\nBase: %s", lib, target, current)
+    logging.info("--------------------")
     result = subprocess.run(["adb",
-                             "-s",
-                             "192.168.1.22:5555",
+                             "-t",
+                             device_id,
                              "push",
                              "--sync",
-                             join(lib, current),
-                             join(lib, target)],
+                             lib/current,
+                             (target/current).parent],
                             capture_output=True)
     assert(result.returncode == 0), result.stdout.decode()
 
-def pull_sync(target, lib, current):
+def pull_sync(device_id, target, lib, current, depth=1):
     """
     Run an adb pull command to sync device with library
     """
-    logging.info("Pulling: %s : %s : %s", join(target, current), join(lib, current), current)
+    if skip_depth_under(lib, current, 0):
+        logging.info("Skipping %s", current)
+        return
+    logging.info("Pulling: %s\nto:\n %s\ncurrent: %s", target, lib, current)
+    logging.info("--------------------")
     # Compare the target and lib using find
     device_files = subprocess.run(["adb",
-                                   "-s",
-                                   "192.168.1.22:5555",
+                                   "-t",
+                                   device_id,
                                    "shell",
                                    "find",
-                                   join(target, current),
+                                   target/current,
                                    "-type", "f"],
                                   capture_output=True)
-    assert(device_files.returncode == 0), device_files.stdout.decode()
-    device_set = { x[len(target):] for x in device_files.stdout.decode().split("\n") }
+    assert(device_files.returncode == 0), device_files.stderr.decode()
+    device_set = { pathlib.Path(x).relative_to(target) for x in device_files.stdout.decode().split("\n") if x != "" }
 
 
     local_files = subprocess.run(["find",
-                                  join(lib, current),
+                                  lib/current,
                                   "-type", "f"],
                                   capture_output=True)
-    assert(local_files.returncode == 0), local_files.stdout.decode()
-    local_set = {x[len(lib):] for x in device_files.stdout.decode().split("\n")}
+    assert(local_files.returncode == 0), local_files.stderr.decode()
+    local_set = {pathlib.Path(x).relative_to(lib) for x in local_files.stdout.decode().split("\n") if x != ""}
 
     missing = device_set - local_set
-    logging.info("%s missing from %s", len(missing), join(lib, current))
+    logging.info("%s missing from %s", len(missing), lib/current)
     # Then copy missing over
     for path in missing:
-        if path[0] == "/":
-            path = path[1:]
-        assert(not exists(join(lib, path)))
+        assert(not (lib/path).exists())
+        if not (lib/path).parent.exists():
+            (lib/path).parent.mkdir()
+
+        logging.info("Copying: %s\nTo: %s", target/path, lib/path)
         result = subprocess.run(["adb",
-                                "-s",
-                                "192.168.1.22:5555",
+                                "-t",
+                                 device_id,
                                 "pull",
-                                 join(target, path),
-                                 join(lib, path)],
+                                 target/path,
+                                 (lib/path)],
                                 capture_output=True)
-        assert(exists(join(lib, path)))
-        assert(result.returncode == 0), result.stdout.decode()
+        assert((lib/path).exists()), lib/path
+        assert(result.returncode == 0), result.stderr.decode()
 
 
 
@@ -150,19 +174,29 @@ def main():
     config = ConfigParser(allow_no_value=True, delimiters='=')
     config.read(abspath(expanduser(args.config)))
 
+    # device_id = "{}:{}".format(config['ADB']['ipaddr'], config['ADB']['PORT'])
+    device_id = args.id
     # Walk the library directory
     if args.to_device:
         func = push_sync
     elif args.from_device:
         func = pull_sync
+    else:
+        raise Exception()
 
+    lib_path    = pathlib.Path(args.library).resolve()
+    target_path = pathlib.Path(config['ADB']['sdcard'], args.target)
+
+    # Curry the args which won't change
     partial_func = partial(func,
-                           shell_device,
-                           adb_device,
-                           join(config['ADB']['sdcard'], args.target),
-                           args.library)
+                           device_id,
+                           target_path,
+                           lib_path,
+                           depth=args.depthskip)
 
-    dfs_dir(args.library,
+    logging.info("DFS for: %s\nto: %s", lib_path, target_path)
+    # Run the walk
+    dfs_dir(lib_path,
             args.wait,
             partial_func)
 
@@ -170,6 +204,7 @@ def main():
 if __name__ == '__main__':
     main()
 
+##-- adb_shell_example
     # from adb_shell.adb_device import AdbDeviceTcp
     # from adb_shell.auth.sign_pythonrsa import PythonRSASigner
     # from ppadb.client import Client
@@ -208,3 +243,4 @@ if __name__ == '__main__':
     # all_files = [x for x in shell_device.shell(instruction).split("\n") if x != ""]
     # logging.info("Found %s files in %s", len(all_files), args.target)
     # print(all_files)
+##-- End adb_shell_example
