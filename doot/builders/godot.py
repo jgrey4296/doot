1fe012ec1e535450ad6c9f0273a1192a6ac5e24a
John Grey <johngrey4296@gmail.com>
2022-12-22 02:52:42 +0000

    [feature] add to basic godot tasks
    --------------------------------------------------
    Warnings:
    
    --------------------------------------------------
    Basic Explanation:
    
    --------------------------------------------------
    Outstanding issues:
    
    --------------------------------------------------
    Notes:

--- doot/builders/godot.py
+++ doot/builders/godot.py
@@ -12,38 +12,85 @@ from doot.utils.general import build_cmd
 ##-- end imports
 # https://docs.godotengine.org/en/stable/tutorials/editor/command_line_tutorial.html
 
+
 def task_godot_check():
-    return { "basename": "godot::check"
-             "actions": []
-            }
+    """
+    Lint all gd scripts in the project
+    """
+    for script in pl.Path().rglob("*.gd"):
+        return { "basename": "godot::check",
+                 "actions": [ "godot --no-window --check-only --script {dependencies}"],
+                 "file_dep" : [ script ],
+                }
 
 def task_godot_version():
-    return { "basename": "godot::version"
-             "actions": []
+    return { "basename": "godot::version",
+             "actions": ["godot --version"]
             }
 
 def task_godot_run():
-    return { "basename": "godot::run"
-             "actions": []
+    """
+    Run a godot script or scene, with debugging or without
+    """
+    def run_cmd(target, debug):
+        ext = pl.Path(target).suffix
+        args = []
+        if debug:
+            args.append("-d")
+
+        match ext:
+            case ".tscn":
+                args.append(target)
+            case ".gd":
+                args.append("--no-window")
+                args.append("--script")
+                args.append(target)
+
+        return "godot " + " ".join(args)
+
+    return { "basename": "godot::run",
+             "actions": ["godot {debug} {scene}"],
+             "params" : [
+                 { "name"    : "target",
+                   "short"   : "t",
+                   "type"    : str,
+                   "default" : "",
+                  },
+                 { "name"    : "debug",
+                   "short"   : "d",
+                   "type"    : bool,
+                   "default" : False,
+                  },
+             ],
             }
 
 def task_godot_test():
-    return { "basename": "godot::test"
-             "actions": []
-            }
-
-def task_godot_debug():
-    return { "basename": "godot::debug"
-             "actions": []
-            }
-
-def task_godot_script():
-    return { "basename": "godot::script"
+    return { "basename": "godot::test",
              "actions": []
             }
 
 def task_godot_build():
-    # export
-    return { "basename": "godot::build"
-             "actions": []
+    """
+    build a godot project
+    """
+    return { "basename": "godot::build",
+             "actions": ["godot --no-window --{type} {target}" + f"{build_dir}/build.dmg"],
+             "params" : [
+                 { "name"    : "target",
+                   "short"   : "t",
+                   "type"    : str,
+                   "default" : "osx",
+                   "choices" : [("Mac OSX", ""),
+                                ("Android", ""),
+                                ],
+                  },
+                 { "name"    : "type",
+                   "long"    : "type",
+                   "type"    : str,
+                   "default" : "export",
+                   "choices" : [ ("export", ""),
+                                 ("export-debug", "")
+                                ]
+                  },
+             ]
             }
