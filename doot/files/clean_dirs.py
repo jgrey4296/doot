8952ede093da8dcdcfa5f4ca1cff383a357b69cf
John Grey <johngrey4296@gmail.com>
2022-12-23 03:45:08 +0000

    [increment] allow forced directory deletion
    --------------------------------------------------
    Warnings:
    
    --------------------------------------------------
    Basic Explanation:
    
    --------------------------------------------------
    Outstanding issues:
    
    --------------------------------------------------
    Notes:

--- doot/files/clean_dirs.py
+++ doot/files/clean_dirs.py
@@ -18,15 +18,26 @@ def clean_target_dirs(task, dryrun):
         print("%s - dryrun removing '%s'" % (task.name, task.targets))
         return
 
+    force_tree = task.meta is not None and "force_clean" in task.meta
+
     for target_s in sorted(task.targets, reverse=True):
         try:
             target = pl.Path(target_s)
-            print("%s - removing '%s'" % (task.name, target))
+            if not target.exists():
+                print("%s - N/A '%s'" % (task.name, target))
+                continue
+
             if target.is_file():
+                print("%s - removing '%s'" % (task.name, target))
                 target.remove()
             elif target.is_dir() and not bool([x for x in target.iterdir() if x.name != ".DS_Store"]):
+                print("%s - removing tree '%s'" % (task.name, target))
+                shutil.rmtree(str(target))
+            elif target.is_dir() and force_tree:
+                print("%s - force removing tree '%s'" % (task.name, target))
                 shutil.rmtree(str(target))
             else:
-                print(f"Dir {target} is not empty: {list(target.iterdir())}")
+                contains = " ".join(str(x) for x in target.iterdir())
+                print("%s - not empty: %s : %s" % (task.name, target, contains))
         except OSError as err:
             print(err)
