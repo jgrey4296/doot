e0a7eb6b0357c0f046760312d823a8198dd91718
John Grey <johngrey4296@gmail.com>
2023-01-18 04:37:20 +0000

    [increment] add type checking to TomlAccess
    --------------------------------------------------
    Warnings:
    
    --------------------------------------------------
    Basic Explanation:
    
    --------------------------------------------------
    Outstanding issues:
    
    --------------------------------------------------
    Notes:

--- doot/toml_access.py
+++ doot/toml_access.py
@@ -15,6 +15,7 @@ from __future__ import annotations
 import abc
 import logging as logmod
 import pathlib as pl
+from types import NoneType
 from copy import deepcopy
 from dataclasses import InitVar, dataclass, field
 from re import Pattern
@@ -22,6 +23,7 @@ from typing import (TYPE_CHECKING, Any, Callable, ClassVar, Final, Generic,
                     Iterable, Iterator, Mapping, Match, MutableMapping,
                     Protocol, Sequence, Tuple, TypeAlias, TypeGuard, TypeVar,
                     cast, final, overload, runtime_checkable)
+from types import UnionType
 from uuid import UUID, uuid1
 from weakref import ref
 
@@ -43,79 +45,129 @@ if TYPE_CHECKING:
 logging = logmod.getLogger(__name__)
 ##-- end logging
 
+from doot.utils.trace_helper import TraceHelper
+
 class TomlAccessError(AttributeError):
     pass
 
 
 class TomlAccessValue:
+    """
+    A Wrapper for guarded access to toml values.
+    you get the value by calling it.
+    Until then, it tracks attribute access,
+    and reports that to TomlAccess when called.
+    It also can type check its value and the value retrieved from the toml data
+    """
 
-    def __init__(self, value):
-        self.value = value
+    def __init__(self, value, types=None, path=None):
+        types = types or "Any"
+        self._value = (value,)
+        self._types = types
+        self._path  = path or []
+
+        if self._types != "Any" and not isinstance(value, self._types):
+            match self._types:
+                case UnionType() as targ:
+                    types_str = repr(targ)
+                case type(__name__=targ):
+                    types_str = targ
+                case _ as targ:
+                    types_str = str(targ)
+            path_str = ".".join(path + ['(' + types_str + ')'])
+            raise TypeError("Toml Value doesn't match declared Type: ", path_str, self._value, self._types).with_traceback(TraceHelper()[5:10])
 
     def __call__(self):
-        if isinstance(self.value, tuple) and len(self.value) == 1:
-            return self.value[0]
-
-        return self.value
+        match self._value, self._path:
+            case (val,), []:
+                return val
+            case (val,), [*path]:
+                path_str = ".".join(path + [f"<{self._types}>"])
+                TomlAccess.missing_paths.append(path_str)
+                return val
+            case val, path:
+                raise TypeError("Unexpected Values found: ", val, path)
 
     def __getattr__(self, attr):
+        self._path.append(attr)
         return self
 
+    def using(self, val):
+        return TomlAccessValue(val, types=self._types, path=self._path)
+
 class TomlAccess:
+    """
+    Provides access to toml data (TomlAccess.load(apath))
+    but as attributes (data.a.path.in.the.data)
+    instead of key access (data['a']['path']['in']['the']['data'])
+
+    while also providing typed, guarded access:
+    data.or_get("test", str | int).a.path.that.may.exist()
+
+    while it can then report missing paths:
+    data._report() -> ['a.path.that.may.exist.<str|int>']
+    """
+
+    missing_paths : ClassVar[list[str]] = []
 
     @staticmethod
     def load(path) -> self:
         logging.info("Creating TomlAccess for %s", str(path))
-        return TomlAccess("root", toml.load(path))
+        return TomlAccess("<root>", toml.load(path))
 
     def __init__(self, path, table, fallback=None):
-        if not isinstance(path, list):
-            path = [path]
-        object.__setattr__(self, "__table", table)
-        object.__setattr__(self, "__path", path)
-        object.__setattr__(self, "__fallback", fallback)
+        assert(isinstance(fallback, (NoneType, TomlAccessValue)))
+        path = path if isinstance(path, list) else [path]
+        super().__setattr__("__table"    , table)
+        super().__setattr__("_path"     , path)
+        super().__setattr__("__fallback" , fallback)
 
-    def or_get(self, val) -> TomlAccessValue:
+    def or_get(self, val, types=None) -> TomlAccessValue:
         """
         use a fallback value in an access chain,
         eg: doot.config.or_get("blah").this.doesnt.exist() -> "blah"
 
         *without* throwing a TomlAccessError
         """
-        path  = object.__getattribute__(self, "__path")[:]
-        table = object.__getattribute__(self, "__table")
-        return TomlAccess(path, table, fallback=(val,))
+        path  = getattr(self, "_path")[:]
+        table = getattr(self, "__table")
+        assert(path == ["<root>"])
+        return TomlAccess(path, table, fallback=TomlAccessValue(val, types=types))
 
     def keys(self):
         table  = object.__getattribute__(self, "__table")
         return table.keys()
 
+    def _report(self) -> list[str]:
+        """
+        Report the paths using default values
+        """
+        return TomlAccess.missing_paths[:]
+
     def __setattr__(self, attr, value):
-        raise TomlAccessError(attr)
+        if attr in getattr(self, "__table"):
+            raise AttributeError(attr)
+        super().__setattr__(attr, value)
 
     # TODO -> getattribute
-    def __getattr__(self, attr) -> TomlAccessValue | str | list | int | float:
-        new_path = object.__getattribute__(self, "__path")[:]
-        table    = object.__getattribute__(self, "__table")
-        fallback = object.__getattribute__(self, "__fallback")
-
-        result = table.get(attr)
-        if result is None and "_" in attr :
-            result = object.__getattribute__(self, "__table").get(attr.replace("_", "-"))
-
-        if result is None and fallback is not None:
-            return TomlAccessValue(fallback)
-
-        if result is None:
-            path_s    = ".".join(new_path)
-            available = "/".join(self.keys())
-            raise TomlAccessError(f"{path_s}.[{attr}] not found, available: {available}")
-
-        if isinstance(result, dict):
-            new_path.append(attr)
-            return TomlAccess(new_path, result, fallback=fallback)
-
-        if fallback is not None:
-            return TomlAccessValue(result)
-
-        return result
+    def __getattr__(self, attr) -> TomlAccessValue | str | list | int | float | bool:
+        table    = getattr(self, "__table")
+        fallback = getattr(self, "__fallback")
+        if fallback:
+            getattr(fallback, attr)
+        match (table.get(attr) or table.get(attr.replace("_", "-"))):
+            case None if fallback is not None:
+                return fallback
+            case None:
+                path     = getattr(self, "_path")[:]
+                path_s    = ".".join(path)
+                available = "/".join(self.keys())
+                raise TomlAccessError(f"{path_s}.[{attr}] not found, available: {available}")
+            case dict() as result:
+                path     = getattr(self, "_path")[:]
+                return TomlAccess(path + [attr], result, fallback=fallback)
+            case _ as result if fallback is not None:
+                # Theres a fallback value, so the result needs to be wrapped so it can be called
+                return fallback.using(result)
+            case _ as result:
+                return result
