370f6d5e3033c0abd26d39eaa92be29f93f0dc82
John Grey <johngrey4296@gmail.com>
2023-01-24 13:30:37 +0000

    [feature] add downloader mixin
    --------------------------------------------------
    Warnings:
    
    --------------------------------------------------
    Basic Explanation:
    
    --------------------------------------------------
    Outstanding issues:
    
    --------------------------------------------------
    Notes:

---
+++ doot/taskslib/files/downloader.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+##-- imports
+from __future__ import annotations
+
+import logging as logmod
+from os import system
+from os.path import split
+from typing import (Any, Callable, ClassVar, Dict, Final, Generic, Iterable,
+                    Iterator, List, Mapping, Match, MutableMapping, Optional,
+                    Sequence, Set, Tuple, TypeVar, Union, cast)
+
+import requests
+import doot
+from doot import tasker
+
+##-- end imports
+
+logging = logmod.getLogger(__name__)
+
+CHECK_AMNT    : Final = doot.config.on_fail(150, int).tool.doot.downloader.check_amnt()
+speak_confirm : Final = tasker.ActionsMixin.say(None, "Found a Large Group of Files, waiting for confirmation")
+
+class DownloaderMixin:
+
+    def download_media(self, media_dir:pl.Path, media:list):
+        """ Download all media mentioned in json files """
+        print("Downloading media %s to: %s", len(media), media_dir)
+        assert(media_dir.is_dir())
+        if not media_dir.exists():
+            media_dir.mkdir()
+
+        remaining = [x for x in media if x is not None and not (media_dir / pl.Path(x).name).exists()]
+
+        if len(remaining) > CHECK_AMNT:
+            speak_confirm.execute()
+            result = input("Continue? [y/n] ")
+            if result != "y":
+                print("Skipping download")
+                return
+
+        scaler = int(len(media) / 100) + 1
+        for i, x in enumerate(media):
+            if i % scaler == 0:
+                logging.info("%s/100", int(i/scaler))
+
+            filename = media_dir / split(x)[1]
+            if filename.exists():
+                continue
+
+            try:
+                request = requests.get(x)
+                with open(filename, 'wb') as f:
+                    f.write(request.content)
+            except Exception as e:
+                logging.warning("Error Downloading: %s", x)
+                logging.warning(str(e))
