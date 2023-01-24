85b79eb4b8078b4ff16b7ecb3d312af9a85001db
John Grey <johngrey4296@gmail.com>
2023-01-24 13:28:30 +0000

    [stub]
    --------------------------------------------------
    Warnings:
    
    --------------------------------------------------
    Basic Explanation:
    
    --------------------------------------------------
    Outstanding issues:
    
    --------------------------------------------------
    Notes:

---
+++ doot/taskslib/builders/_erlang.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+"""
+
+"""
+##-- imports
+
+##-- end imports
+
+##-- default imports
+from __future__ import annotations
+
+import types
+import abc
+import datetime
+import enum
+import functools as ftz
+import itertools as itz
+import logging as logmod
+import pathlib as pl
+import re
+import time
+from copy import deepcopy
+from dataclasses import InitVar, dataclass, field
+from typing import (TYPE_CHECKING, Any, Callable, ClassVar, Final, Generic,
+                    Iterable, Iterator, Mapping, Match, MutableMapping,
+                    Protocol, Sequence, Tuple, TypeAlias, TypeGuard, TypeVar,
+                    cast, final, overload, runtime_checkable)
+from uuid import UUID, uuid1
+from weakref import ref
+
+##-- end default imports
+
+##-- logging
+logging = logmod.getLogger(__name__)
+##-- end logging
+
+from doot import tasker, globber
+
+# https://rebar3.org/docs/getting-started/
+# https://hex.pm/docs/usage
+# https://hexdocs.pm/mix/Mix.html
+
+class ErlangBuild(tasker.DootTasker):
+    pass
