26692743e48a0adb9e70bb5f7d535a3ddc1bc8a1
John Grey <johngrey4296@gmail.com>
2023-01-25 16:56:02 +0000

    [refactor]
    --------------------------------------------------
    Warnings:
    
    --------------------------------------------------
    Basic Explanation:
    
    --------------------------------------------------
    Outstanding issues:
    
    --------------------------------------------------
    Notes:

---
+++ bkmkorg/bibtex/load_save.py
@@ -0,0 +1,128 @@
+#!/usr/bin/env python3
+"""
+
+"""
+##-- imports
+
+##-- end imports
+
+##-- default imports
+from __future__ import annotations
+
+import abc
+import datetime
+import enum
+import functools as ftz
+import itertools as itz
+import logging as logmod
+import pathlib as pl
+import re
+import time
+import types
+from copy import deepcopy
+from dataclasses import InitVar, dataclass, field
+from typing import (TYPE_CHECKING, Any, Callable, ClassVar, Final, Generic,
+                    Iterable, Iterator, Mapping, Match, MutableMapping,
+                    Protocol, Sequence, Tuple, TypeAlias, TypeGuard, TypeVar,
+                    cast, final, overload, runtime_checkable)
+from uuid import UUID, uuid1
+from weakref import ref
+
+##-- end default imports
+
+##-- logging
+logging = logmod.getLogger(__name__)
+##-- end logging
+
+import bibtexparser as b
+import doot
+from bibtexparser import customization as c
+from bibtexparser.bparser import BibTexParser
+
+__all__ = ["BibLoadSaveMixin"]
+
+class OverrideDict(dict):
+    """
+    A Simple dict that doesn't error if a key isn't found.
+    Used to avoid UndefinedString Exceptions in bibtex parsing
+    """
+
+    def __getitem__(self, k):
+        if k not in self:
+            logging.warning("Adding string to override dict: %s", k)
+            self[k] = k
+        return k
+
+class BibLoadSaveMixin:
+
+    def bc_load_db(self, files:list[pl.Path], fn:callable=None, db=None) -> BibtexDatabase:
+        return self._parse_bib_files(files, fn=fn, db=db)
+
+    def bc_db_to_str(self, db, fn:callable, lib_root) -> str:
+        writer = b_write.JGBibTexWriter()
+        for entry in db.entries:
+            fn(entry, lib_root)
+
+        return writer.write(self.current_db)
+
+    def bc_prepare_entry_for_write(self, entry, lib_root) -> None:
+        """ convert processed __{field}'s into strings in {field},
+        removing the the __{field} once processed
+        """
+
+        for field in entry.keys():
+            if field[:2] != "__":
+                continue
+
+            match field:
+                case "__tags":
+                    entry["tags"] = self._join_tags(entry[field])
+                case "__paths":
+                    entry.update(self._path_strs(entry[field]))
+                case "__authors":
+                    pass
+                case "__editors":
+                    pass
+                case _:
+                    pass
+            del entry[field]
+
+    def _join_tags(self, tagset) -> str:
+        return ",".join(tagset)
+
+    def _path_strs(self, pathdict, lib_root) -> dict:
+        results = {}
+        for field, path in pathdict.items():
+            if not path.is_relative_to(lib_root):
+                results[field] = str(path)
+
+            assert(field not in results)
+            rel_path = path.relative_to(lib_root)
+            results[field] = rel_path
+
+        return results
+
+    def _make_parser(self, func):
+        bparser = BibTexParser(common_strings=False)
+        bparser.ignore_nonstandard_types = False
+        bparser.homogenise_fields        = True
+        bparser.customization            = func
+        bparser.expect_multiple_parse     = True
+        return bparser
+
+    def _parse_bib_files(self, bib_files:list[pl.Path], fn=None, db=None):
+        """ Parse all the bibtext files into a shared database """
+        bparser = self._make_parser(fn)
+        if db is None:
+            logging.info("Creating new database")
+            db = b.bibdatabase.BibDatabase()
+
+        db.strings = OverrideDict()
+
+        bparser.bib_database = db
+        for x in bib_files:
+            with open(x, 'r') as f:
+                logging.info("Loading bibtex: %s", x)
+                bparser.parse_file(f, partial=True)
+        logging.info("Bibtex loaded: %s entries", len(db.entries))
+        return db
---
+++ bkmkorg/twitter/__test/test_file_processing.py
@@ -0,0 +1,41 @@
+#https://docs.python.org/3/library/unittest.html
+# https://docs.python.org/3/library/unittest.mock.html
+
+from os.path import splitext, split
+
+import unittest
+import unittest.mock as mock
+
+import logging
+
+
+class FileProcessingTests(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        logmod.getLogger('').setLevel(logmod.WARNING)
+        LOG_FILE_NAME = "log.{}".format(splitext(split(__file__)[1])[0])
+
+        file_h = logmod.FileHandler(LOG_FILE_NAME, mode='w')
+        file_h.setLevel(logmod.DEBUG)
+
+        console = logmod.StreamHandler()
+        console.setLevel(logmod.WARNING)
+
+        logging = logmod.getLogger(__name__)
+        logging.setLevel(logmod.DEBUG)
+        logging.addHandler(console)
+        logging.addHandler(file_h)
+
+
+    def setUp(self):
+        return 1
+
+    def tearDown(self):
+        return 1
+
+    #----------
+    # use testcase snippet
+    # mock.Mock / MagicMock
+    # create_autospec
+    # @patch(' ') / with patch.object(...)
