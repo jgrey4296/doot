fe67b5ca9e3b4aea42552cda3bd0114ab65f7ab8
John Grey <johngrey4296@gmail.com>
2023-11-07 14:48:09 +0000

    [add]: clean command implementation

--- doot/cmds/clean_cmd.py
+++ doot/cmds/clean_cmd.py
@@ -59,26 +59,79 @@ logging = logmod.getLogger(__name__)
 printer = logmod.getLogger("doot._printer")
 
 import doot
+import shutil
 from doot._abstract import Command_i
 from collections import defaultdict
+
+clean_locs = doot.config.on_fail([], list).commands.clean.locs()
 
 
 class CleanCmd(Command_i):
     """
       Runs either a general clean command, or a specific task clean command
+
     """
     _name      = "clean"
-    _help      = []
+    _help      = [
+        "Called with a -t[arget]={task}, will delete locations listed in that task's toml spec'd `clean_targets`",
+        "Without a target, will delete locations listed in doot.toml's: `commands.clean.locs`",
+        "Directories will *not* be deleted unless -r[ecursive] is passed",
+        ]
 
     @property
     def param_specs(self) -> list:
         return super().param_specs + [
-            self.make_param(name="target", type=str, default=""),
+            self.make_param(name="target", type=str, default=None),
             self.make_param(name="recursive", type=bool, default=False)
             ]
 
     def __call__(self, tasks:dict, plugins:dict):
-        if bool(doot.args.on_fail(None).cmd.args.target()):
-            printer.info("TODO: Clean targeting %s", doot.args.on_fail.cmd.args.target)
-        else:
-            printer.info("TODO: General Clean")
+        clean_target = doot.args.on_fail(None).cmd.args.target()
+        if clean_target is None:
+            printer.info("- No Clean Target Specified.")
+            existing_clean_targets = list(filter(lambda x: x.exists(), [doot.locs[x] for x in clean_locs]))
+            self._gen_clean(existing_clean_targets)
+        elif clean_target in tasks:
+            self._task_clean(tasks[clean_target])
+        elif clean_target not in tasks:
+            printer.error("Specified Target not found in task list: %s", clean_target)
+
+    def _task_clean(self, task):
+        printer.info("Cleaning Task: %s", task.name)
+        task_clean_locs = task.extra.on_fail([], list).clean_locs()
+        existing_clean_targets = list(filter(lambda x: x.exists(), [doot.locs[x] for x in task_clean_locs]))
+        self._gen_clean(existing_clean_targets)
+
+
+    def _gen_clean(self, existing_clean_targets):
+        if not bool(existing_clean_targets):
+            printer.info("- No Viable Clean Locations.")
+            return
+
+        printer.info("- Clean will *delete* the following locations:")
+        printer.info("")
+        for loc in existing_clean_targets:
+            printer.info("-- %s", str(loc))
+
+        printer.info("")
+        match input(":-- WARNING: Do you want to clean? Yes/_: "):
+            case "Yes":
+                for loc in existing_clean_targets:
+                    match input(f"--- Delete {loc} ? Y/_: "):
+                        case "Y" if loc.is_dir() and doot.args.cmd.args.recursive:
+                            printer.info("---- Deleting Recursively")
+                            shutil.rmtree(loc)
+                        case "Y" if loc.is_dir():
+                            printer.info("---- Deleting Non-Recursively")
+                            try:
+                                loc.rmdir()
+                            except OSError:
+                                printer.info("---- Location is not empty, to delete recursively, pass -r to clean")
+                        case "Y" if loc.is_file():
+                            printer.info("---- Deleting")
+                            loc.unlink()
+                        case _:
+                            printer.info("---- Skipping")
+
+            case _:
+                printer.info("Not cleaning.")
