142ca14e81d858637482e5f4562ea46fbba16919
John Grey <johngrey4296@gmail.com>
2024-02-23 23:36:37 +0000

    [bump]: version 0.5.2 -> 0.6.0

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 ![doot](https://github.com/jgrey4296/doot/assets/5943270/170a5631-6175-4d92-8d66-e26aa2c2e472)
 # doot
-Version : 0.5.2.5.1  
+Version : 0.6.0.5.2.5.1  
 Author  : John Grey  
 Date    : 2022-12-09  
 
--- doot/__init__.py
+++ doot/__init__.py
@@ -27,7 +27,7 @@ logging = logmod.getLogger(__name__)
 printer         = logmod.getLogger("doot._printer")
 
 # Global, single points of truth:
-__version__          : Final[str]         = "0.5.2"
+__version__          : Final[str]         = "0.6.0"
 
 config               : TG.TomlGuard       = TG.TomlGuard() # doot config
 locs                 : DootLocData        = DootLocations(pl.Path()) # registered locations
