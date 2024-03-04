8e83fd41710169189072a36749114d5a8cdce865
John Grey <johngrey4296@gmail.com>
2024-03-04 05:06:40 +0000

    [refactor]: postbox to use tasknames

--- doot/actions/postbox.py
+++ doot/actions/postbox.py
@@ -56,46 +56,42 @@ class _DootPostBox:
     default_subkey                        = "_default"
 
     @staticmethod
-    def put(key, val, subkey=None):
-        subkey = subkey or _DootPostBox.default_subkey
+    def put(key:DootTaskName, val):
+        subbox = key.last()
+        box    = key.root()
         match val:
             case None | [] | {} | dict() if not bool(val):
                 pass
             case list() | set():
-                _DootPostBox.boxes[key][subkey] += val
+                _DootPostBox.boxes[box][subbox] += val
             case _:
-                _DootPostBox.boxes[key][subkey].append(val)
+                _DootPostBox.boxes[box][subbox].append(val)
 
     @staticmethod
-    def put_from(state, val, subkey=None):
-        """
-        utility to add to a postbox using the state, instead of calculating the root yourself
-        """
-        key    = TASK_NAME.to_type(None, state).root()
-        subkey = subkey or _DootPostBox.default_subkey
-        _DootPostBox.put(key, val, subkey=subkey)
-
-    @staticmethod
-    def get(key, subkey=Any) -> list:
-        match subkey:
+    def get(key:DootTaskName, subkey=Any) -> list:
+        box    = key.root()
+        subbox = key.last()
+        match subbox:
             case "" | "-":
-                return _DootPostBox.boxes[key][_DootPostBox.default_subkey][:]
+                return _DootPostBox.boxes[box][_DootPostBox.default_subkey][:]
             case x if x == Any:
-                return _DootPostBox.boxes[key][_DootPostBox.default_subkey][:]
+                return _DootPostBox.boxes[box][_DootPostBox.default_subkey][:]
             case "*" | None:
-                return _DootPostBox.boxes[key].copy()
+                return _DootPostBox.boxes[box].copy()
             case _:
-                return _DootPostBox.boxes[key][subkey]
+                return _DootPostBox.boxes[box][subbox]
 
     @staticmethod
-    def clear_box(key, subkey=Any):
-        match subkey:
+    def clear_box(key):
+        box    = key.root()
+        subbox = key.last()
+        match subbox:
             case x if x == Any:
-                _DootPostBox.boxes[key][_DootPostBox.default_subkey] = []
-            case None:
-                _DootPostBox.boxes[key] = defaultdict(list)
+                _DootPostBox.boxes[box][_DootPostBox.default_subkey] = []
+            case False:
+                _DootPostBox.boxes[box] = defaultdict(list)
             case _:
-                _DootPostBox.boxes[key][subkey] = []
+                _DootPostBox.boxes[box][subkey] = []
 
 @doot.check_protocol
 class PutPostAction(Action_p):
@@ -104,19 +100,23 @@ class PutPostAction(Action_p):
     The arguments of the action are held in self.spec
     'args' are pushed to the default subbox
     'kwargs' are pushed to the kwarg subbox
+
+    eg: {do="post.put", args=["{key}", "{key}"], subbox="{key}"}
     """
 
     @DootKey.kwrap.args
     @DootKey.kwrap.kwargs
-    def __call__(self, spec, state, args, kwargs) -> dict|bool|None:
-        target = TASK_NAME.to_type(spec, state).root()
+    @DootKey.kwrap.taskname
+    def __call__(self, spec, state, args, kwargs, _basename) -> dict|bool|None:
+        target = basename.root().subtask(True)
         for statekey in args:
             data = DootKey.make(statekey).to_type(spec, state)
             _DootPostBox.put(target, data)
 
-        for subkey,statekey in kwargs.items():
+        for box,statekey in kwargs.items():
+            box  = basename.root().subtask(box)
             data = DootKey.make(statekey).to_type(spec, state)
-            _DootPostBox.put(target, data, subkey=subkey)
+            _DootPostBox.put(box, data)
 
 
 @doot.check_protocol
@@ -125,23 +125,19 @@ class GetPostAction(Action_p):
       Read data from the inter-task postbox of a task tree
       The arguments of the action are held in self.spec
 
-      from=task
-      kwarg=subkey -> get the subbox and update task state as kwarg
-      kwarg=""     -> get the default subbox
-      kwarg="*"    -> get the entire box dict
+      stateKey="group::task.{subbox}"
+      eg: data="bib::format.-"
     """
 
-    @DootKey.kwrap.types("from", hint={"type_":str|None})
     @DootKey.kwrap.kwargs
     def __call__(self, spec, state, _from, kwargs) -> dict|bool|None:
-        target_box = _from or TASK_NAME.to_type(spec, state).root()
-
         updates = {}
         for key,subkey in kwargs.items():
             if key == FROM_KEY:
                 pass
-            actual_key = DootKey.make(key, explicit=True).expand(spec, state)
-            updates[actual_key] = _DootPostBox.get(target_box, subkey=subkey)
+            state_key          = DootKey.make(key, explicit=True).expand(spec, state)
+            target_box         = DootTaskName.from_str(subkey)
+            updates[state_key] = _DootPostBox.get(target_box)
 
         return updates
 
