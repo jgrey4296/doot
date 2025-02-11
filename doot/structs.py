bafd3b9c7093e0818d08eb53c18c360f990ecdf4
John Grey <johngrey4296@gmail.com>
2025-02-11 18:58:01 +0000

    [fix]: dkey import

--- doot/structs.py
+++ doot/structs.py
@@ -28,13 +28,13 @@ from uuid import UUID, uuid1
 ##-- end builtin imports
 
 from jgdv.cli.param_spec import ParamSpec
+from jgdv.structs.dkey import DKeyed, DKey, SingleDKey, MultiDKey, NonDKey
 from jgdv.structs.locator import JGDVLocator, Location
-from doot._structs.dkey import DKey, SingleDKey, MultiDKey, NonDKey
+import doot._structs.dkey
 from doot._structs.action_spec import ActionSpec
 from doot._structs.artifact import TaskArtifact
 from doot._structs.stub import TaskStub, TaskStubPart
 from doot._structs.task_name import TaskName
 from doot._structs.task_spec import TaskSpec
 from doot._structs.trace import TraceRecord
-from doot._structs.dkey import DootKeyed as DKeyed
 from doot._structs.inject_spec import InjectSpec
