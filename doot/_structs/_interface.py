3234a11526a5c10f8df523af45c4e5c8209cdc65
John Grey <johngrey4296@gmail.com>
2025-02-24 09:04:47 +0000

    [chore]:

---
+++ doot/_structs/_interface.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python3
+"""
+
+
+"""
+# ruff: noqa:
+
+# Imports:
+from __future__ import annotations
+
+# ##-- stdlib imports
+import datetime
+import enum
+import functools as ftz
+import itertools as itz
+import logging as logmod
+import pathlib as pl
+import re
+import time
+import types
+import collections
+import contextlib
+import hashlib
+from copy import deepcopy
+from uuid import UUID, uuid1
+from weakref import ref
+import atexit # for @atexit.register
+import faulthandler
+# ##-- end stdlib imports
+
+# ##-- types
+# isort: off
+import abc
+import collections.abc
+from typing import TYPE_CHECKING, cast, assert_type, assert_never
+from typing import Generic, NewType
+# Protocols:
+from typing import Protocol, runtime_checkable
+# Typing Decorators:
+from typing import no_type_check, final, override, overload
+# from dataclasses import InitVar, dataclass, field
+# from pydantic import BaseModel, Field, model_validator, field_validator, ValidationError
+
+if TYPE_CHECKING:
+    from jgdv import Maybe
+    from typing import Final
+    from typing import ClassVar, Any, LiteralString
+    from typing import Never, Self, Literal
+    from typing import TypeGuard
+    from collections.abc import Iterable, Iterator, Callable, Generator
+    from collections.abc import Sequence, Mapping, MutableMapping, Hashable
+
+##--|
+
+# isort: on
+# ##-- end types
+
+##-- logging
+logging = logmod.getLogger(__name__)
+##-- end logging
+
+# Vars:
+
+# Body:
